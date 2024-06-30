967a4bbdab7356b5fba0cd38d6e606848dde3d77
clairehansen33 <olivia.hansen@petco.com>
2024-06-30 09:40:38 -0700

    revamping home page design

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,22 @@ function onClickMenu() {
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
 
+// Change style of navbar on scroll
+
+window.onscroll = function () {
+  myFunction();
+};
+function myFunction() {
+  var navbar = document.getElementById("header");
+  if (
+    document.body.scrollTop > 76 ||
+    document.documentElement.scrollTop > 76
+  ) {
+    navbar.className = "header" + " nav-bar";
+  } else {
+    navbar.className = navbar.className.replace(" nav-bar", "");
+  }
+}
 
 /* slider */
 // Glideco v1.6.0
@@ -232,3 +248,34 @@ $('.tab-menu li a').removeClass('active');
   $("#"+target).fadeIn('slow').siblings(".tab-box").hide();
   return false;
 });
+
+// MODALS
+
+// Get the modal
+var modal = document.getElementById("petco-hp-desktop-modal");
+var modal = document.getElementById("petco-hp-mobile-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("petco-hp-desktop-btn");
+var btn = document.getElementById("petco-hp-mobile-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-petco-hp-desktop")[0];
+var span = document.getElementsByClassName("close-petco-hp-mobile")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
